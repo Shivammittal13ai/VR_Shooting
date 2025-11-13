6520bbfe0eccddbdeb3118933bbb9a562f50f605
Shivam mittal <iamsmittal13@gmail.com>
2025-11-13 23:42:23 +0530

    Add files via upload

---
+++ Scripts/MouseMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    public float mouseSensitivity = 500f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    public float topClamp = -90f;
+    public float bottomClamp = 90f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Locking the cursor to the middle of the screen & making it invisible
+        Cursor.lockState = CursorLockMode.Locked;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Geting the mouse i/p
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //Rotation around the x axis (Look up & down)
+        xRotation -= mouseY;
+
+        //Clamp the rotation
+        xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
+
+        //Rotation around the y axis (Look left & right)
+        yRotation += mouseX;
+
+        //Apply rotations to our transform
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+
+        
+    }
+}
---
+++ Scripts/Weapon.cs
@@ -0,0 +1,182 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Weapon : MonoBehaviour
+{
+
+    //Shooting
+    public bool isShooting, readyToShoot;
+    bool allowReset = true;
+    public float shootingDelay = 2f;
+
+    //Burst
+    public int bulletsPerBurst = 3;
+    public int burstBulletsLeft;
+
+    //Spread
+    public float spreadIntensity;
+
+    //Bullet
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletVelocity = 30;
+    public float bulletPrefabLifeTime = 3f; //seconds
+
+    public GameObject muzzleEffect;
+    private Animator animator;
+
+    //Loading
+    public float realoadTime;
+    public int magazineSize, bulletsLeft;
+    public bool isReloading;
+
+    public enum ShootingMode
+    {
+        Single,
+        Burst,
+        Auto
+    }
+
+    public ShootingMode currentShootingMode;
+
+    private void Awake()
+    {
+        readyToShoot = true;
+        burstBulletsLeft = bulletsPerBurst;
+        animator = GetComponent<Animator>();
+
+        bulletsLeft = magazineSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bulletsLeft == 0 && isShooting)
+        {
+            SoundManager.Instance.emptyMagazineSoundM1911.Play();
+        }
+
+        if (currentShootingMode == ShootingMode.Auto)
+        {
+            //Holding Down Left Mouse Button
+            isShooting = Input.GetKeyDown(KeyCode.Mouse0);
+        }
+        else if(currentShootingMode == ShootingMode.Single || currentShootingMode == ShootingMode.Burst)
+        {
+            //Clicking Left Mouse Button Once
+            isShooting = Input.GetKeyDown(KeyCode.Mouse0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && isReloading == false)
+        {
+            Reload();
+        }
+
+        if (readyToShoot && isShooting == false && isReloading == false && bulletsLeft <= 0)
+        {
+            Reload();
+        }
+        
+        if (readyToShoot && isShooting && bulletsLeft > 0)
+        {
+            burstBulletsLeft = bulletsPerBurst;
+            FireWeapon();
+        }
+
+        if (AmmoManager.Instance.ammoDisplay != null)
+        {
+            AmmoManager.Instance.ammoDisplay.text = $"{bulletsLeft/bulletsPerBurst}/{magazineSize/bulletsPerBurst}";
+        }
+    }
+
+    private void FireWeapon()
+    {
+        bulletsLeft--;
+
+        muzzleEffect.GetComponent<ParticleSystem>().Play();
+        animator.SetTrigger("RECOIL");
+
+        SoundManager.Instance.shootingSoundM1911.Play();
+
+        readyToShoot = false;
+
+        Vector3 shootingDirection = CalculateDirectionAndSpread().normalized;
+
+        //Instantiate the bullet
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+
+        //Shoot the bullet
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse);
+
+        //Destroy he bullet after sometime
+        StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifeTime));
+
+        //Checking if we are done shooting
+        if (allowReset)
+        {
+            Invoke("ResetShot", shootingDelay);
+            allowReset = false;
+        }
+
+        //Burst Mode
+        if (currentShootingMode == ShootingMode.Burst && burstBulletsLeft > 1)
+        {
+            burstBulletsLeft--;
+            Invoke("FireWeapon", shootingDelay);
+        }
+
+    }
+
+    private void Reload()
+    {
+        SoundManager.Instance.reloadingSoundM1911.Play();
+
+        isReloading = true;
+        Invoke("ReloadCompleted", realoadTime);
+    }
+
+    private void ReloadCompleted()
+    {
+        bulletsLeft = magazineSize;
+        isReloading = false;
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        allowReset = true;
+    }
+
+
+    public Vector3 CalculateDirectionAndSpread()
+    {
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+        {
+            targetPoint = hit.point;
+        }
+        else
+        {
+            targetPoint = ray.GetPoint(100);
+        }
+
+        Vector3 direction = targetPoint - bulletSpawn.position;
+
+        float x = UnityEngine.Random.Range(-spreadIntensity, spreadIntensity);
+        float y = UnityEngine.Random.Range(-spreadIntensity, spreadIntensity);
+
+        return direction + new Vector3(x, y, 0);
+    }
+
+
+    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float bulletPrefabLifeTime)
+    {
+        yield return new WaitForSeconds(bulletPrefabLifeTime);
+        Destroy(bullet);
+    }
+}
